38f06dc7036d1211c88046bc35d150d8e903d68f
uni-fied <devadiroot@gmail.com>
2022-06-28 11:57:31 +0700

    Tittle & description updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Jurnal Kodinganku!',
+  tagline: 'Jurnal kodinganku',
   url: 'https://github.com',
   baseUrl: '/my-dokku-website/',
   onBrokenLinks: 'throw',
